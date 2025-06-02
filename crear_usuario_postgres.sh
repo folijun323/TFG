d0bd8a5131ccc7a2210d48ae071c6eb8411eb94f
folijun323 <folijun323@fp.iesromerovargas.com>
2025-06-02 18:02:20 +0200

    Add files via upload

---
+++ crear_usuario_postgres.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Ejecutar comandos dentro de psql como usuario postgres
+sudo -i -u postgres psql <<EOF
+DO
+\$do\$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_catalog.pg_roles WHERE rolname = 'curro'
+   ) THEN
+      CREATE ROLE curro WITH SUPERUSER CREATEDB CREATEROLE LOGIN PASSWORD '1';
+   END IF;
+END
+\$do\$;
+EOF
